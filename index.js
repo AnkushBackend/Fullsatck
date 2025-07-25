fee8a6304bb8755ae4b53038569f10870654d024
Ankush <rawatshab8744@gmail.com>
2025-07-26 00:40:52 +0530

    Added frontend CORS URL

--- index.js
+++ index.js
@@ -6,18 +6,8 @@ const methodOverride = require("method-override");
 const cors = require("cors");
 
 const app = express();
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://frontend-55ki.onrender.com");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
-
 app.use(cors({
     origin: "https://frontend-55ki.onrender.com",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
